42fb385d8fc3c20e59f6a1b38f202ae2020e7291
Scott Pickering <scottpickering31@gmail.com>
2023-11-09 17:42:11 +0000

    Added LargeMovieCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import LargeMovieCard from "./LargeMovieCard";
+import "./App.css";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [searchLetter, setSearchLetter] = useState("s");
+  const [dataObj, setDataObj] = useState("Search");
+  const [chosenMovie, setChosenMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [detailedMovies, setDetailedMovies] = useState("");
+
+  useEffect(() => {
+    searchMovies("Harry Potter");
+  }, []);
+
+  const searchMovies = async (title) => {
+    setIsLoading(true);
+    const response = await fetch(`${API_URL}&${searchLetter}=${title}`);
+    const data = await response.json();
+    setMovies(data[dataObj]);
+    setIsLoading(false);
+  };
+
+  const movieChoice = async (movie) => {
+    setSearchLetter("t");
+    setDataObj("");
+    setChosenMovie(movie);
+
+    setIsLoading(true);
+    // Fetch detailed information for the chosen movie
+    const detailedResponse = await fetch(
+      `${API_URL}&t=${movie.Title}&plot=full`
+    );
+    const detailedData = await detailedResponse.json();
+    setDetailedMovies(detailedData);
+    console.log(detailedData);
+    setIsLoading(false);
+  };
+
+  const resetSearch = () => {
+    setChosenMovie(null);
+    setDataObj("Search");
+    setSearchLetter("s");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Scott's Movie Finder</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src="https://raw.githubusercontent.com/gist/adrianhajdin/997a8cdf94234e889fa47be89a4759f1/raw/f13e5a9a0d1e299696aa4a0fe3a0026fa2a387f7/search.svg"
+          alt="search"
+          onClick={() => {
+            searchMovies(searchTerm) && resetSearch();
+          }}
+        />
+      </div>
+
+      {chosenMovie ? (
+        <LargeMovieCard detailedMovies={detailedMovies} />
+      ) : movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              movie={movie}
+              imageClick={() => movieChoice(movie)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          {isLoading ? (
+            <p className="loading">Loading...</p>
+          ) : (
+            <h2>No movies found</h2>
+          )}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/LargeMovieCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const LargeMovieCard = ({ detailedMovies }) => {
+  const { Released, Plot, Title, Year, Runtime, Poster, Ratings } =
+    detailedMovies;
+  // Render details of the chosen movie here
+  return (
+    <div className="detailed-description">
+      <h2>{Title}</h2>
+      <p>{Plot}</p>
+      <p>{Released}</p>
+      <img src={Poster} />
+      <p>{Runtime}</p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
+
+export default LargeMovieCard;
